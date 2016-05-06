6fe9a5db90ec682a672bd39cb66d8c1e98fbe82f
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-05-06 14:17:10 -0300

    service tests

---
+++ lib/tsdesktop/service_test.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from tsdesktop import service
+
+class TestService(TestCase):
+    srvcs = None
+
+    def setUp(self):
+        if self.srvcs is None:
+            self.srvcs = [c() for _, c in service.srvMap.items()]
+
+    def test_service_class_map_name(self):
+        for name in service.srvMap.keys():
+            s = service.srvMap.get(name)()
+            self.assertEqual(s.name, name)
+
+    def test_service_detach(self):
+        for s in self.srvcs:
+            if s.name == 'httpd':
+                self.assertFalse(s.detach)
+            else:
+                self.assertTrue(s.detach)
+
+    def test_service_preChecks(self):
+        for s in self.srvcs:
+            s.preChecks()
+
+    def test_service_runArgs(self):
+        for s in self.srvcs:
+            if s.name == 'mysqld':
+                self.assertListEqual(s.runArgs,
+                    ['-v', s._datadir+':/var/lib/mysql'])
+            else:
+                self.assertListEqual(s.runArgs, [])
