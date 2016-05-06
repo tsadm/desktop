ee3fbdc49cda96314f4fa250d7ba82ffe339241a
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-05-06 12:58:14 -0300

    do not show buildinfo author

--- lib/tsdesktop/version.py
+++ lib/tsdesktop/version.py
@@ -5,7 +5,7 @@ VERSION = "16.05"
 def println():
     s = "tsdesktop v{}".format(VERSION)
     if not buildinfo.ID is None:
-        s = "{} ({} {} {})".format(s, buildinfo.ID[:7], buildinfo.DATE, buildinfo.AUTHOR)
+        s = "{} ({} {})".format(s, buildinfo.ID[:7], buildinfo.DATE)
     print(s)
 
 if __name__ == '__main__':
