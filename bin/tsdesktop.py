be09ae036a8f719495901f6092f3d4aef5f421e0
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-05-06 02:45:19 -0300

    do not add ziplib to sys.path

--- bin/tsdesktop.py
+++ bin/tsdesktop.py
@@ -6,8 +6,5 @@ from os.path import dirname, abspath, join
 libdir = abspath(join(dirname(__file__), '..', 'lib'))
 sys.path.insert(0, libdir)
 
-ziplib = abspath(join(libdir, 'tsdesktop.zip'))
-sys.path.insert(0, ziplib)
-
 from tsdesktop import cmd
 sys.exit(cmd.main())
