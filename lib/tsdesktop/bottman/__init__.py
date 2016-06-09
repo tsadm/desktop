9584dd520619991ab01679f2c59ef7418cdcdf21
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-08 21:14:04 -0300

    dockman services

--- lib/tsdesktop/bottman/__init__.py
+++ lib/tsdesktop/bottman/__init__.py
@@ -5,38 +5,41 @@ from tsdesktop import dockman
 
 app = Bottle()
 
-
+# -- dashboard
 @app.route('/')
 def index():
-    tdata = {
-        'dockmanServices': [
-            {'name': 'mysqld'},
-            {'name': 'faked'},
-        ],
-    }
-    return render('index', **tdata)
+    services = [
+        {'name': 'mysqld'},
+        {'name': 'faked'},
+    ]
+    return render('index', dockmanServices=services)
 
 
+# -- static content
 @app.route('/static/<fpath:path>')
 def static(fpath):
     return staticFile(fpath)
 
 
+# -- error 404
 @app.error(404)
 def error404(err):
     return render('error', err=err)
 
 
+# -- error 500
 @app.error(500)
 def error500(err):
     return render('error', err=err)
 
 
+# -- settings
 @app.route('/settings')
 def settings():
     return render('settings')
 
 
+# -- docker
 @app.route('/docker')
 def docker():
     try:
