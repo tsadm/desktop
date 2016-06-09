db6807d2698c508549786c64bd50c1d418419f6f
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-08 22:33:32 -0300

    sorted list of services

--- lib/tsdesktop/bottman/views/dashboard.py
+++ lib/tsdesktop/bottman/views/dashboard.py
@@ -2,7 +2,7 @@ from tsdesktop.dockman import services
 from ..utils import render
 
 def view():
-    sl = [s() for s in services.classMap.values()]
+    sl = [services.classMap[k]() for k in sorted(services.classMap.keys())]
     return render('dashboard', dockmanServices=sl)
 
 def init(app):
