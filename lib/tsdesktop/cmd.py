755accfd7c68f3306de8942c42d2c71979bac7b4
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-08 21:47:04 -0300

    fix cmd args

--- lib/tsdesktop/cmd.py
+++ lib/tsdesktop/cmd.py
@@ -4,10 +4,19 @@ from tsdesktop import config, version, bottman
 
 def _parseArgs():
     parser = ArgumentParser(description='tsadm desktop client')
-    parser.add_argument('-V', '--version', action='store_true', help='show version and build info')
-    parser.add_argument('--http', nargs='?', type=int, const=3680, metavar="PORT", help='start web interface (default port: 3680)')
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug mode')
-    parser.add_argument('-D', '--config', action='store_true', help='dump config')
+    parser.add_argument('-V', '--version',
+        action='store_true',
+        help='show version and build info',
+    )
+    parser.add_argument('-p', '--port',
+        nargs='?', type=int,
+        default=3680,
+        help='TCP port to bind to (default: 3680)',
+    )
+    parser.add_argument('-d', '--debug',
+        action='store_true', help='enable debug mode')
+    parser.add_argument('-D', '--config',
+        action='store_true', help='dump config')
     return parser
 
 
@@ -17,15 +26,11 @@ def main():
     parser = _parseArgs()
     args = parser.parse_args()
 
-    if args.http:
-        return bottman.app.run(host='localhost', port=args.http, debug=args.debug)
-
-    elif args.config:
+    if args.config:
         return config.cmd()
 
     elif args.version:
         return 0
 
     else:
-        parser.print_help()
-        return 1
+        return bottman.app.run(host='localhost', port=args.port, debug=args.debug)
