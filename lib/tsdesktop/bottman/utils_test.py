5db16790ee03fd08219603a8718289e84e1ada88
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-10 03:10:13 -0300

    test utils render

--- lib/tsdesktop/bottman/utils_test.py
+++ lib/tsdesktop/bottman/utils_test.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
-from .utils import staticFile, render
+from .utils import staticFile
+from .views import dashboard
 import bottle
 
-class TestUtils(TestCase):
+
+class StaticFile(TestCase):
 
     def test_staticFile(self):
         sf = staticFile('w3.css')
@@ -15,3 +17,10 @@ class TestUtils(TestCase):
     def test_staticFileNotFound(self):
         sf = staticFile('w3.cssNOT_FOUND')
         self.assertEqual(sf.status_code, 404)
+
+
+class Render(TestCase):
+
+    def test_render(self):
+        v = dashboard.view()
+        self.assertEqual(len(v), 2378)
