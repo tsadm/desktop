d533544cb1a09b2ab5ecca377f871c7bdec9730e
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-11 01:27:01 -0300

    check error pages lines

--- lib/tsdesktop/bottman/__test.py
+++ lib/tsdesktop/bottman/__test.py
@@ -10,15 +10,15 @@ class Views(TSDesktopTest):
 
     def test_error400(self):
         v = error400(bottle.HTTPError(400, 'fake error'))
-        self.assertEqual(len(v), 1331)
+        self.assertLinesContains(v, '<h3>ERROR - 400')
 
     def test_error404(self):
         v = error404(bottle.HTTPError(404, 'fake error'))
-        self.assertEqual(len(v), 1329)
+        self.assertLinesContains(v, '<h3>ERROR - 404')
 
     def test_error500(self):
         v = error500(bottle.HTTPError(500, 'fake error'))
-        self.assertEqual(len(v), 1341)
+        self.assertLinesContains(v, '<h3>ERROR - 500')
 
     def test_settings(self):
         from tsdesktop import config
