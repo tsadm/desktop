cf409f7611c3527ae88068a62f976ac6da12f1d7
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-16 16:12:52 -0300

    osx specific utils

---
+++ osx/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -x
+
+test -x brew || {
+    echo "ERROR brew command line tools seems not to be installed!!"
+    echo "ERROR check Homebrew web site for installing: http://brew.sh"
+    exit 1
+}
+
+brew update
+
+brew tap caskroom/cask
+brew cask install virtualbox
+
+for pkg in python python3 docker docker-machine; do
+    brew install $pkg
+done
+
+docker-machine create --driver virtualbox default
+
+exit 0
