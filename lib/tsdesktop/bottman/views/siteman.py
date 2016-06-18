3310ee6840813f16d8ae3e1e93ce6282581d2eb4
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-18 00:52:45 -0300

    open site and redirect to site view

--- lib/tsdesktop/bottman/views/siteman.py
+++ lib/tsdesktop/bottman/views/siteman.py
@@ -5,6 +5,11 @@ from bottle import request, HTTPError, redirect
 from tsdesktop.siteman import openSite
 
 
+# -- site info
+def siteView(name):
+    return name
+
+
 # -- open site
 def siteOpen():
     name = request.params.get('site_name', None)
@@ -12,10 +17,10 @@ def siteOpen():
     if not ok:
         return HTTPError(400, 'invalid site name: '+name)
     docroot = request.params.get('site_docroot', None)
-    err, siteId = openSite(docroot)
+    err = openSite(name, docroot)
     if err is not None:
         return HTTPError(400, 'could not open site: '+str(err))
-    return redirect('/sites/'+siteId)
+    return redirect('/site/'+name+'/view')
 
 
 # -- sites index
@@ -25,5 +30,6 @@ def sites():
 
 # -- init views
 def init(app):
+    app.route('/site/<name>/view', callback=siteView)
     app.route('/site/open', method='POST', callback=siteOpen)
     app.route('/sites', callback=sites)
--- lib/tsdesktop/siteman/__init__.py
+++ lib/tsdesktop/siteman/__init__.py
@@ -2,26 +2,23 @@ from os import path
 
 
 class Site:
-    ID = None
+    name = None
     docroot = None
 
-    def __init__(self, docroot):
+    def __init__(self, name, docroot):
+        self.name = name
         self.docroot = docroot
-        self.ID = self._genID()
-
-    def _genID(self):
-        return self.docroot
 
     def load(self):
         inifile = path.join(path.dirname(self.docroot), '.tsdesktop.ini')
 
 
-def openSite(docroot):
+def openSite(name, docroot):
     dpath = path.abspath(docroot)
     if not path.exists(dpath):
-        return ('dir not found', None)
+        return 'path not found'
     elif not path.isdir(dpath):
-        return ('not a dir', None)
-    site = Site(dpath)
+        return 'not a dir'
+    site = Site(name, dpath)
     site.load()
-    return (site.ID, 'lalala')
+    return None
