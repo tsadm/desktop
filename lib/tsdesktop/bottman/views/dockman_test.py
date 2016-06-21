b1f3ced7bd14a5ff8711295535d7ea6dd1f43c3f
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-21 12:02:26 -0300

    test dockman pull image

--- lib/tsdesktop/bottman/views/dockman_test.py
+++ lib/tsdesktop/bottman/views/dockman_test.py
@@ -1,5 +1,6 @@
 from tsdesktop.testing import TSDesktopTest
 from .dockman import dockman
+from bottle import HTTPResponse
 
 class Views(TSDesktopTest):
 
@@ -7,3 +8,11 @@ class Views(TSDesktopTest):
         v = dockman()
         self.assertLinesContains(v,
             '<div id="dockman_mysqld" class="w3-modal">')
+
+    def test_pullImage(self):
+        with self.assertRaises(HTTPResponse) as cm:
+            dockman('mysqld', 'pull-image')
+        resp = cm.exception
+        self.assertEqual(resp.status_code, 302)
+        loc = resp.get_header('Location')
+        self.assertEqual(loc, 'http://127.0.0.1/dockman')
