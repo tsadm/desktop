d5f7a9ac243c05884766c3d0ed73cb216b1d2317
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-22 20:02:52 -0300

    dockman test getClient

--- lib/tsdesktop/dockman/__test.py
+++ lib/tsdesktop/dockman/__test.py
@@ -1,1 +1,10 @@
+from tsdesktop.testing import TSDesktopTest
+from tsdesktop import dockman
+import docker
 
+class Client(TSDesktopTest):
+
+    def test_getClient(self):
+        dockman._cli = None
+        cli = dockman.getClient()
+        self.assertIsInstance(cli, docker.Client)
