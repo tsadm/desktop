d65f8ede038f9ce9c8765c245fce1c3ec1c304d7
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-24 18:55:07 -0300

    test site load

--- lib/tsdesktop/siteman/__test.py
+++ lib/tsdesktop/siteman/__test.py
@@ -8,3 +8,13 @@ class Site(TSDesktopTest):
         self.assertEqual(s.name, 'fake.test')
         self.assertEqual(s.docroot, '/var/www/html')
         self.assertEqual(str(s), '<Site: fake.test>')
+
+    def test_SiteNoDocroot(self):
+        s = siteman.Site('fake.test', '/nonexistent')
+        err = s.load()
+        self.assertEqual(err, 'path not found')
+
+    def test_SiteNotDir(self):
+        s = siteman.Site('fake.test', '/usr/bin/env')
+        err = s.load()
+        self.assertEqual(err, 'not a dir')
