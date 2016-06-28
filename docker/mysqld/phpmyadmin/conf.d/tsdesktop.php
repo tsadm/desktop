95b510462be0aeda0b6b0545d8f4781d53093457
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-28 18:11:54 -0300

    update dockerfiles - mysqld phpmyadmin

---
+++ docker/mysqld/phpmyadmin/conf.d/tsdesktop.php
@@ -0,0 +1,7 @@
+<?php
+$cfg['Servers'][$i]['controluser'] = 'tsdesktop';
+$cfg['Servers'][$i]['controlpass'] = 'tsdesktop';
+
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['user'] = 'tsdesktop';
+$cfg['Servers'][$i]['password'] = 'tsdesktop';
