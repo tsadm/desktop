7fd0eb8e9887ca3935e07d43850ffad56f045418
Jeremías Casteglione <jeremias@tincan.co.uk>
2016-06-28 18:55:29 -0300

    initialize phpmyadmin installation

--- docker/mysqld/phpmyadmin/conf.d/tsdesktop.php
+++
@@ -1,7 +0,0 @@
-<?php
-$cfg['Servers'][$i]['controluser'] = 'tsdesktop';
-$cfg['Servers'][$i]['controlpass'] = 'tsdesktop';
-
-$cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['user'] = 'tsdesktop';
-$cfg['Servers'][$i]['password'] = 'tsdesktop';
